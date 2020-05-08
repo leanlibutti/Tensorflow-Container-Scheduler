1890ea33603abd77dfd709f7010e86219862d5a3
Francisco Igual <figual@ucm.es>
2020-05-08 11:38:25 +0200

    Change launch script to get values for env. variables from number of CPUs in the container when launching

--- launch_all.sh
+++ launch_all.sh
@@ -1,4 +1,8 @@
-export MIN_ENV_THREADS=2
-export MAX_ENV_THREADS=2
+ncpus=`cat /proc/cpuinfo | grep processor | wc -l`
+
+export MIN_ENV_THREADS=$ncpus
+export MAX_ENV_THREADS=$ncpus
 
 python /data/tensorflow_examples/keras_example_tf2.py
+
+exit
